62400e1295bda9b0b678ee9abff4f13d6d2a85c0
Thakor DakshKumar <anshuxthakor@gmail.com>
2026-09-14 23:02:45 +0530

    http package

--- 00/server.js
+++ 00/server.js
@@ -1,9 +1,20 @@
 let http = require('http');
 
 let server = http.createServer((req, res) => {
-  res.end("Hello Client, I am a Server");
+
+  if (req.url === '/home') {
+    res.end('This is the HOME page');
+  }
+
+  if (req.url === '/about') {
+    res.end('This is the ABOUT page');
+  }
+
+  if (req.url === '/users') {
+    res.end('This is the USERS page');
+  }
 });
 
 server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log('Server is running on port 3000');
 });
